e4bd0ec5d142dd71e043be2c330b569314c49b57
haru-0903 <harujava0903@gmail.com>
2025-08-27 12:02:11 +0900

    hi

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,8 @@ public class Main {
             // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
             System.out.println("i = " + i);
             System.out.println("이승명 갔다왔ㅇ");
+            System.out.println("미소귀요미");
+
         }
     }
 }
